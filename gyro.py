a15b3ebc45a033f6e1863af6d9c83fb86183af98
Roberto Alsina <roberto.alsina@gmail.com>
2017-07-11 09:48:42 -0300

    Renamed static folder

--- gyro.py
+++ gyro.py
@@ -1,2 +1,7 @@
-import sanic
+from sanic import Sanic
+from sanic.response import json
 
+app = Sanic(__name__)
+
+# Serves files from the static folder to the URL /static
+app.static('/_static', './_static')
