f5c85ca62b9426444f1e706bcd933e1fddbab643
Roberto Alsina <roberto.alsina@gmail.com>
2017-07-11 13:26:09 -0300

    only serve on localhost

--- gyro.py
+++ gyro.py
@@ -36,4 +36,4 @@ async def post_wikiword(request, word='Index'):
         return json({'message': 'Saved'})
     
 
-app.run(host="0.0.0.0", port=8000, debug=True)
+app.run(host="127.0.0.1", port=8000, debug=True)
