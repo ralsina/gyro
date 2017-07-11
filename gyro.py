c8c9d727e92e9ba10adcf205efefdb8f15377501
Roberto Alsina <roberto.alsina@gmail.com>
2017-07-11 14:53:49 -0300

    Search

--- gyro.py
+++ gyro.py
@@ -1,8 +1,10 @@
+import glob
+import json
 import os
 from string import Template
 
 from sanic import Sanic
-from sanic.response import html, json
+from sanic import response
 
 app = Sanic(__name__)
 
@@ -25,7 +27,7 @@ async def get_wikiword(request, word='Index'):
         content = 'No content yet'
 
     page = template.substitute(content=content)
-    return html(page)
+    return response.html(page)
 
 
 @app.route('/<word:[A-z0-9]*>', methods=['POST'])
@@ -35,7 +37,21 @@ async def post_wikiword(request, word='Index'):
     markdown = request.form['markdown'][0]
     with open(path, mode='w', encoding='utf-8') as outf:
         outf.write(markdown)
-        return json({'message': 'Saved'})
+        reindex_site()  # FIXME: this can be made more efficient.
+        return response.json({'message': 'Saved'})
 
 
-app.run(host="127.0.0.1", port=8000, debug=True)
+def reindex_site():
+    docs = []
+    for doc in glob.glob(os.path.join('pages', '*.md')):
+        docname = os.path.splitext(os.path.basename(doc))[0]
+        with open(doc, encoding='utf8') as inf:
+            text = inf.read()
+            docs.append({'name': docname, 'text': text})
+    index_path = os.path.join('_static', 'index.js')
+    with open(index_path, mode='w', encoding='utf-8') as outf:
+        json.dump(docs, outf)
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8000, debug=True)
