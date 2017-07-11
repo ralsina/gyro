3e5054f2f67b0fb21cef65c84a2a1dc1dc536eff
Roberto Alsina <roberto.alsina@gmail.com>
2017-07-11 15:29:08 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'gyro',
+  packages = ['gyro'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A small, nice, personal wiki',
+  author = 'Roberto Alsina',
+  author_email = 'roberto.alsina@gmail.com',
+  url = 'https://github.com/ralsina/gyro', # use the URL to the github repo
+)
