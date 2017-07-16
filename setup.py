49d30029eb22d4a7c992de239f3dd410f94ad748
Roberto Alsina <roberto.alsina@gmail.com>
2017-07-16 15:07:19 -0300

    0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'gyro',
   scripts = ['gyro.py'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'A small, nice, personal wiki',
   author = 'Roberto Alsina',
   author_email = 'roberto.alsina@gmail.com',
